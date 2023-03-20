a49350c4319d9c93544cf97bc6f9b65f4e24baf7
Huang <mail@huanghong.top>
2023-03-20 21:57:40 +0800

    project init

---
+++ huang-nacos-learn/src/main/java/com/huang/discovery/Main.java
@@ -0,0 +1,20 @@
+package com.huang.discovery;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Time 2023-03-20 21:07
+ * Created by Huang
+ * className: Main
+ * Description:
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class,args);
+    }
+}
